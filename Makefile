23ac51b058221de481d7a9e5089d1096cd95e888
Ubuntu <ubuntu@ip-172-31-25-31.ec2.internal>
2024-02-14 21:31:53 +0000

    modifications

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 SHELL := /bin/bash
 
-IMAGE_NAME=myimage
+IMAGE_NAME=helloworld-express
 DOCKER_REPO=myusername
 DOCKERFILE=./app/Dockerfile
 DOCKER_TAG=1.0
@@ -11,33 +11,33 @@ DOCKER_PASSWORD ?= $(shell read -s -p "Enter Docker password: " p && echo $$p)
 
 #install kind
 install-kind:
-    @echo "Installing kind..."
+	@echo "Installing kind..."
 	./install-kind.sh 
 
 #build docker image
 docker-build:
-    docker build -t $(IMAGE_NAME):$(DOCKER_TAG) -f $(DOCKERFILE) .
+	docker build -t $(IMAGE_NAME):$(DOCKER_TAG) -f $(DOCKERFILE) .
 
 #login to docker
 docker-login:
-    @echo "$(DOCKER_PASSWORD)" | docker login -u "$(DOCKER_USERNAME)" --password-stdin
+	@echo "$(DOCKER_PASSWORD)" | docker login -u "$(DOCKER_USERNAME)" --password-stdin
 
 #push image to docker
 docker-push: docker-login
-    docker tag $(IMAGE_NAME):$(DOCKER_TAG) $(DOCKER_REPO)/$(IMAGE_NAME):$(DOCKER_TAG)
-    docker push $(DOCKER_REPO)/$(IMAGE_NAME):$(DOCKER_TAG)
+	docker tag $(IMAGE_NAME):$(DOCKER_TAG) $(DOCKER_REPO)/$(IMAGE_NAME):$(DOCKER_TAG)
+	docker push $(DOCKER_REPO)/$(IMAGE_NAME):$(DOCKER_TAG)
 
 #initialize terraform
 init:
-    cd ./terraform && terraform init
+	cd ./terraform && terraform init
 
 #run terraform plan
 plan: init
-    cd ./terraform && terraform plan
+	cd ./terraform && terraform plan
 
 #run terraform apply
 apply: init
-    cd ./terraform && terraform apply
+	cd ./terraform && terraform apply
 
 #destroy terraform
 destroy:
--- install-kind.sh
+++ install-kind.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-#Install kind from binaries
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.21.0/kind-linux-amd64
-chmod +x ./kind
-sudo mv ./kind /usr/local/bin/kind
-
-#create k8s cluster
-kind create cluster
-
-#set kubeconfig env var for terraform to confer from
-export KUBE_CONFIG_PATH=$(cat "${HOME}/.kube/config")
-
-#check if kubectl is already installed
-if [ ! command -v kubectl &> /dev/null ]; then
+#check if kubectl is not installed and install it
+if ! command -v kubectl  &> /dev/null; then
     echo "kubectl is not installed. Downloading..."
     #install kubectl
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo  curl -LO "https://dl.k8s.io/release/$(sudo curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 else
     echo "kubectl is already installed."
 fi
 
+#Install kind from binaries
+sudo curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.21.0/kind-linux-amd64
+sudo chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+#create k8s cluster
+kind create cluster
+
+#use kind context
+kubectl cluster-info --context kind-kind
+
 #install helm
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+sudo chmod +x  get_helm.sh
+sudo ./get_helm.sh
+
+#set kubeconfig env var for terraform to confer from
+export KUBE_CONFIG_PATH=$(cat "${HOME}/.kube/config") 
 
 echo "kind cluster successfully bootstrapped"
